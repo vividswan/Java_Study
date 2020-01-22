467dc9d70bd5712f150e73904938d7b65672403f
vividswan <vividswan@naver.com>
2020-01-23 04:51:51 +0900

    Create EmployeeTest.java

---
+++ objectoriented/EmployeeTest.java
@@ -0,0 +1,15 @@
+package objectoriented;
+
+
+public class EmployeeTest {
+
+	public static void main(String[] args) { // main에서 시작
+		Employee employeePark = new Employee(); // new => 새로운 클래스를 만들겠다는 선언
+		employeePark.employeeId = 1234;
+		employeePark.employeeName = "vividswan";
+		employeePark.employeePosition ="master";
+		
+		employeePark.showInformation(); // 1234, vividswan, master 출력
+	}
+
+}
