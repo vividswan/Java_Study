1e31de239f6b71d6a7aa684e4acdbb341f8f7d0b
vividswan <vividswan@naver.com>
2020-02-05 20:13:54 +0900

    Docs reference directory

--- objectoriented/reference/Employee.java
+++ objectoriented/reference/Employee.java
@@ -1,11 +1,11 @@
-package objectoriented;
+package objectoriented.reference;
 
 public class Employee { // public => 접근제어자, 클래스명은 항상 대문자
 	public int employeeId; // employee의 속성(propery, attribute)들
 	public String employeeName;
 	public String employeePosition;
-	
+
 	public void showInformation() { // employee의 메서드
-		System.out.println(employeeId+", "+employeeName+", "+employeePosition);
+		System.out.println(employeeId + ", " + employeeName + ", " + employeePosition);
 	}
 }
--- objectoriented/reference/EmployeeTest.java
+++ objectoriented/reference/EmployeeTest.java
@@ -1,5 +1,4 @@
-package objectoriented;
-
+package objectoriented.reference;
 
 public class EmployeeTest {
 
@@ -7,8 +6,8 @@ public class EmployeeTest {
 		Employee employeePark = new Employee(); // new => 새로운 클래스를 만들겠다는 선언
 		employeePark.employeeId = 1234;
 		employeePark.employeeName = "vividswan";
-		employeePark.employeePosition ="master";
-		
+		employeePark.employeePosition = "master";
+
 		employeePark.showInformation(); // 1234, vividswan, master 출력
 	}
 
