6c95cea69ef71dd454fbb5b83e2290ba2c42e6e3
vividswan <vividswan@naver.com>
2020-02-11 14:52:17 +0900

    Update Person.java

--- objectoriented/thispractice/Person.java
+++ objectoriented/thispractice/Person.java
@@ -1,6 +1,12 @@
 package objectoriented.thispractice;
 
 public class Person {
+
+    // this => 객체지향에서 자기자신을 나타냄(super => 자신의 상위 클래스)
+    // 1. 자기 자신의 메모리를 가리킴
+    // 2. 생성자에서 다른 생성자를 호출함
+    // 3. 인스턴스 자신의 주소를 반환
+
     String name;
     int age;
 
