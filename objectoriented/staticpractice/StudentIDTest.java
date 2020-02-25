524f30ebb9df03af373f0947d5c5172758f0c28b
vividswan <vividswan@naver.com>
2020-02-25 11:37:59 +0900

    Update StudentIDTest.java

--- objectoriented/staticpractice/StudentIDTest.java
+++ objectoriented/staticpractice/StudentIDTest.java
@@ -7,14 +7,14 @@ public class StudentIDTest {
 		// 인스턴스의 생성과는 상관없이 static 변수 사용 가능!
 
 		Student studentPark = new Student("Park");
-		System.out.println(studentPark.getSerialNum()); // 1001
+		System.out.println(Student.getSerialNum()); // 1001
 		// static 변수는 참조변수를 access하는게 아니고 class를 access
 		// studentPark을 Student로 바꿔도 똑같다.
 
 		Student studentKim = new Student("Kim");
 
-		System.out.println(studentKim.getSerialNum()); // 1002
-		System.out.println(studentPark.getSerialNum()); // 1002
+		System.out.println(Student.getSerialNum()); // 1002
+		System.out.println(Student.getSerialNum()); // 1002
 		// 둘 다 1002 => 두 개가 같은 메모리를 공유하고있다.
 
 		// 데이터영역 : serialNum, 스택 메모리 : studentPark, studentKim, 힙: studentPark 인스턴스,
