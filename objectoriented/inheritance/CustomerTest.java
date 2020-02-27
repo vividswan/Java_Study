5de603d8f344c864a1a98859e9b846b51746cc69
vividswan <vividswan@naver.com>
2020-02-27 21:06:34 +0900

    Update CustomerTest.java

--- objectoriented/inheritance/CustomerTest.java
+++ objectoriented/inheritance/CustomerTest.java
@@ -4,17 +4,18 @@ public class CustomerTest {
 
     public static void main(String[] args) {
 
-        Customer customerLee = new Customer();
-        customerLee.setCustomerName("Lee");
-        customerLee.setCustomerID(10010);
+        Customer customerLee = new Customer(10010, "Lee");
+        // customerLee.setCustomerName("Lee");
+        // customerLee.setCustomerID(10010);
         customerLee.bonusPoint = 1000;
         System.out.println(customerLee.ShowCustomerInfo());
 
-        VIPCustomer customerKim = new VIPCustomer();
-        customerKim.setCustomerName("KIM");
-        customerKim.setCustomerID(10020);
+        VIPCustomer customerKim = new VIPCustomer(100020, "KIM");
+        // customerKim.setCustomerName("KIM");
+        // customerKim.setCustomerID(10020);
         customerKim.bonusPoint = 10000;
         System.out.println(customerKim.ShowCustomerInfo());
+
     }
 
 }
