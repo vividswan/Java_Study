4b25355fbf79251a9c5ff607a8acedc0232322a2
vividswan <vividswan@naver.com>
2020-03-01 23:24:53 +0900

    Create GoldCustomer.java

---
+++ objectoriented/polymorphism/GoldCustomer.java
@@ -0,0 +1,20 @@
+package objectoriented.polymorphism;
+
+public class GoldCustomer extends Customer{
+	
+	
+	double salesRatio;
+	
+	public GoldCustomer(int customerID, String customerName) {
+		super(customerID, customerName);
+		
+		customerGrade="GOLD";
+		bonusRatio=0.02;
+		salesRatio=0.1;
+	}
+	
+	public int carcPrice(int price) {
+		bonusPoint += price * bonusRatio;
+		return price - (int)(price*salesRatio);
+	}
+}
