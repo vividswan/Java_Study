15c0df85a3fb2dd112c56559914b7bba479ecf74
vividswan <vividswan@naver.com>
2020-03-04 07:39:00 +0900

    Create MainBoardPlay.java

---
+++ objectoriented/templatemethod/MainBoardPlay.java
@@ -0,0 +1,19 @@
+package objectoriented.template;
+
+public class MainBoardPlay {
+
+	public static void main(String[] args) {
+		
+		Player player = new Player();
+		player.play(1);
+		
+		AdvancedLevel aLevel = new AdvancedLevel();
+		player.upGradeLevel(aLevel);
+		player.play(2);
+		
+		SuperLevel sLevel = new SuperLevel();
+		player.upGradeLevel(sLevel);
+		player.play(3);
+	}
+
+}
