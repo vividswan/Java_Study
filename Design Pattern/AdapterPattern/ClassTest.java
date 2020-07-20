5cfe6fca94cd19d9fb9eddd51116dd43816d8008
vividswan <vividswan@naver.com>
2020-07-20 11:43:04 +0900

    Create ClassTest.java

---
+++ Design Pattern/AdapterPattern/ClassTest.java
@@ -0,0 +1,11 @@
+public class ClassTest {
+
+	public static void main(String[] args) {
+		AdapterClassA adapterClassA = new AdapterClassA();
+		AdapterClassB adapterClassB = new AdapterClassB();
+
+		adapterClassA.runClass();
+		adapterClassB.runClass();
+	}
+
+}
